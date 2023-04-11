6c18204cf1de198dd3b871846c1f5fa7c34e0d9a
Dafter <DafterPlay@mail.ru>
2023-04-11 22:52:18 +0300

    fix

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const newGameButton = document.querySelector('.new_game__button');
 const connectButton = document.querySelector('.connect__button');
 const mainMenuPanels = document.querySelectorAll('.main_menu__panel');
+const mainMenuFlexBox = document.querySelector('.main_menu');
 
 const toggleButton = (evt) => {
   panel = mainMenuPanels[Number(evt.target.dataset.id)];
@@ -9,3 +10,13 @@ const toggleButton = (evt) => {
 
 newGameButton.addEventListener('click', toggleButton);
 connectButton.addEventListener('click', toggleButton);
+
+const handleResize = () => {
+  const flexBoxHeight = mainMenuFlexBox.scrollHeight;
+  const screenHeight = mainMenuFlexBox.clientHeight;
+  mainMenuFlexBox.style.justifyContent = flexBoxHeight > screenHeight ? 'flex-start' : 'center'
+};
+
+new ResizeObserver(() => {
+  handleResize();
+}).observe(mainMenuFlexBox);
