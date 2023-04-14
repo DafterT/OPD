bb2beb15da49957e5d4fbdeed60b80d4aa3d0596
Sol646 <solodovnik.in@edu.spbstu.ru>
2023-04-14 16:44:48 +0300

    Changes to be committed: 	modified:   index.html 	modified:   main.js 	modified:   style.css

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const newGameButton = document.querySelector('.new_game__button');
 const connectButton = document.querySelector('.connect__button');
 const mainMenuPanels = document.querySelectorAll('.main_menu__panel');
 const mainMenuFlexBox = document.querySelector('.main_menu');
+const panelButtons = document.querySelectorAll('.panel_button');
+const gameScreen = document.querySelector('.game_screen');
 
 const toggleButton = (evt) => {
   panel = mainMenuPanels[Number(evt.target.dataset.id)];
@@ -14,9 +16,20 @@ connectButton.addEventListener('click', toggleButton);
 const handleResize = () => {
   const flexBoxHeight = mainMenuFlexBox.scrollHeight;
   const screenHeight = mainMenuFlexBox.clientHeight;
-  mainMenuFlexBox.style.justifyContent = flexBoxHeight > screenHeight ? 'flex-start' : 'center'
+  mainMenuFlexBox.style.justifyContent = flexBoxHeight > screenHeight ? 'flex-start' : 'center';
 };
 
 new ResizeObserver(() => {
   handleResize();
 }).observe(mainMenuFlexBox);
+
+panelButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    mainMenuFlexBox.classList.add('hide_animation');
+    mainMenuFlexBox.addEventListener('animationend', () => {
+      mainMenuFlexBox.style.display = 'none';
+      gameScreen.style.display = 'grid';
+      gameScreen.classList.add('appear_animation');
+    })
+  });
+});
