4d94cffd77f7d9d12bb0d85ee3c8d3b4c120de04
Sol646 <90034701+Sol646@users.noreply.github.com>
2023-06-05 14:39:22 +0300

    license cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -1,3 +1,8 @@
+/* 
+Copyright 2023 Солодовник Игорь Николаевич, Симоновский Даниил Леонидович, Козырев Даниил Владимирович, Ануфриева Виктория Дмитриевна
+Licensed under the Apache License, Version 2.0 
+*/
+
 const cookieMessage = document.querySelector('.messages_cookies');
 const closeCookieMessage = cookieMessage.querySelector('.messages_cookies-close');
 
