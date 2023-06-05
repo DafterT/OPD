d971ef6640a36900adb2135d6087912b3275730f
Sol646 <90034701+Sol646@users.noreply.github.com>
2023-06-05 14:38:11 +0300

    license main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+/* 
+Copyright 2023 Солодовник Игорь Николаевич, Симоновский Даниил Леонидович, Козырев Даниил Владимирович, Ануфриева Виктория Дмитриевна
+Licensed under the Apache License, Version 2.0 
+*/
+
 import { switchTheme, setThemeFromCookie } from './switch-theme.js';
 import { startMain } from './main-menu.js';
 import { generateCode, getData } from './parse-data.js';
