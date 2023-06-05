c57448eab34ca440e3caf7faddeb885513816864
Sol646 <90034701+Sol646@users.noreply.github.com>
2023-06-05 14:38:58 +0300

    license copy-text.js

--- js/copy-text.js
+++ js/copy-text.js
@@ -1,3 +1,8 @@
+/* 
+Copyright 2023 Солодовник Игорь Николаевич, Симоновский Даниил Леонидович, Козырев Даниил Владимирович, Ануфриева Виктория Дмитриевна
+Licensed under the Apache License, Version 2.0 
+*/
+
 const popupElement = document.querySelector('.popup');
 
 const doAnimationPopup = () => {
